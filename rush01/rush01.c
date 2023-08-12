9aea6baa083bda9006dd8906c168333830fb956f
Lui Ebina <luebina@c1r23s8.42tokyo.jp>
2023-08-12 19:22:15 +0900

    done rush01

---
+++ rush01/rush01.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luebina <luebina@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 17:24:18 by rhashizu          #+#    #+#             */
+/*   Updated: 2023/08/12 19:21:50 by luebina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_rush(int x, char head, char middle, char end);
+void	rush(int x, int y);
+void	ft_putchar(char c);
+
+void	print_rush(int x, char head, char middle, char end)
+{
+	int	count_x;
+
+	count_x = 1;
+	while (count_x <= x)
+	{
+		if (count_x == 1)
+		{
+			ft_putchar(head);
+		}
+		else if (count_x == x)
+		{
+			ft_putchar(end);
+		}
+		else
+		{
+			ft_putchar(middle);
+		}
+		count_x++;
+	}
+	write(1, " \n", 2);
+}
+
+void	rush(int x, int y)
+{
+	int	count_y;
+
+	if (x > 0 || y > 0)
+	{
+		count_y = 1;
+		while (count_y <= y)
+		{
+			if (count_y == 1)
+			{
+				print_rush(x, '/', '*', '\\');
+			}
+			else if (count_y == y)
+			{
+				print_rush(x, '\\', '*', '/');
+			}
+			else
+			{
+				print_rush(x, '*', ' ', '*');
+			}
+			count_y++;
+		}
+	}
+}
