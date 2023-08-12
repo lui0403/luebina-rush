01ca564862c5c9f4ca66bc7f8a73b185017c433a
Lui Ebina <luebina@c1r23s8.42tokyo.jp>
2023-08-12 18:04:17 +0900

    fixed norm

--- rush02/rush02.c
+++ rush02/rush02.c
@@ -1,39 +1,58 @@
-// #include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luebina <luebina@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/12 14:04:07 by maskoyam          #+#    #+#             */
+/*   Updated: 2023/08/12 18:03:28 by luebina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar();
+void	ft_putchar(char c);
 
-// rush関数、引数はint x, int y
-void rush(int x, int y) {
-	// printf("x=%d, y=%d \n", x, y);
-	// xかyが0だった場合は何もしないでreturn
-    if (x <= 0 || y <= 0) {
-        return;
-    }
+void	if_func(int x, int y, int i, int j)
+{
+	if ((i == 1 && j == 1) || (i == 1 && j == x))
+	{
+		ft_putchar('A');
+	}
+	else if ((i == y && j == x) || (i == y && j == 1))
+	{
+		ft_putchar('C');
+	}
+	else if ((i == 1 || i == y) || (j == 1 || j == x))
+	{
+		ft_putchar('B');
+	}
+	else
+	{
+		ft_putchar(' ');
+	}
+}
 
-    int i = 1;
-    while (i <= y) {
-        int j = 1;
-        while (j <= x) {
-			// printf("i=%d, j=%d, \n", i, j);
-			// 4つ端を表すif文。これを元にほかの問題も考えていく
-            // if ((i == 1 && j == 1) || (i == 1 && j == x) || (i == y && j == 1) || (i == y && j == x))
-			 if ((i == 1 && j == 1) || (i == 1 && j == x)){
-                // write(1, "A", 1);
-                ft_putchar('A');
-            } else if ((i == y && j == x) || (i == y && j == 1)) {
-                // write(1, "C", 1);
-                ft_putchar('C');
-            } else if ((i == 1 || i == y) || (j == 1 || j == x)) {
-                // write(1, "B", 1);
-                ft_putchar('B');
-            } else {
-                // write(1, " ", 1);
-                ft_putchar(' ');
-            }
-            j++;
-        }
-        write(1, "\n", 1);
-        i++;
-    }
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	if (x <= 0 || y <= 0)
+	{
+		return ;
+	}
+	i = 1;
+	while (i <= y)
+	{
+		j = 1;
+		while (j <= x)
+		{
+			if_func(x, y, i, j);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
 }
