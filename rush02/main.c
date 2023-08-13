f452e882da32802e2b546bc89b4cf10d8bb96b89
Lui Ebina <luebina@c1r26s8.42tokyo.jp>
2023-08-13 11:50:40 +0900

    fix for commandline

--- rush02/main.c
+++ rush02/main.c
@@ -6,14 +6,52 @@
 /*   By: luebina <luebina@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 19:07:06 by luebina           #+#    #+#             */
-/*   Updated: 2023/08/12 19:08:09 by luebina          ###   ########.fr       */
+/*   Updated: 2023/08/13 11:49:44 by luebina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	rush(int x, int y);
 
-int	main(void)
+int custom_atoi(const char *str) {
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+
+    if (str[0] == '-')
+	{
+        sign = -1;
+        i++;
+    }
+	else if (str[0] == '+')
+	{
+        i++;
+    }
+    while (str[i] != '\0')
+	{
+        if (str[i] >= '0' && str[i] <= '9')
+		{
+            result = result * 10 + (str[i] - '0');
+            i++;
+        }
+		else
+		{
+            break;
+        }
+    }
+
+    return result * sign;
+}
+
+int	main(int argc, char const *argv[])
 {
-	rush(5, 3);
+	if (argc != 3)
+	{
+		return 1;
+	}
+	int x = custom_atoi(argv[1]);
+    int y = custom_atoi(argv[2]);
+	rush(x, y);
 	return (0);
 }
